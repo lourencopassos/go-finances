bf43e57eeeb6e5301b09da58b6fb390bd7700672
Lourenço Passos <lo.passos93@gmail.com>
2022-01-05 20:25:01 -0300

    feat: add new props to highlight cards

--- src/components/HighlightCard/index.tsx
+++ src/components/HighlightCard/index.tsx
@@ -9,17 +9,35 @@ import {
   LastTransaction
 } from './styles';
 
-export function HighLightCard() {
+interface HighLightCardProps {
+  title: string;
+  amount: string;
+  lastTransaction: string;
+  type: 'up' | 'down' | 'total';
+}
+
+const icon = {
+  up: 'arrow-up-circle',
+  down: 'arrow-down-circle',
+  total: 'dollar-sign',
+};
+
+export function HighLightCard({
+  title,
+  amount,
+  lastTransaction,
+  type,
+}: HighLightCardProps) {
   return (
-    <Container>
-      <Header>
-        <Title>Entrada</Title>
-        <Icon name='arrow-up-circle' />
+    <Container type={type}>
+      <Header >
+        <Title type={type}>{title}</Title>
+        <Icon name={icon[type]} type={type}/>
       </Header>
 
       <Footer>
-        <Amount>R$ 17.400,00</Amount>
-        <LastTransaction>última entrada dia 13 de abril</LastTransaction>
+        <Amount type={type}>{amount}</Amount>
+        <LastTransaction type={type}>{lastTransaction}</LastTransaction>
       </Footer>
     </Container>
   );
