69232dab77524515075db0edf300e4224f457212
Lourenço Passos <lo.passos93@gmail.com>
2022-02-16 08:23:51 -0300

    style: activity indicator

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import { ActivityIndicator } from 'react-native';
 import { HighLightCard } from '../../components/HighlightCard';
 import {
   TransactionCard,
@@ -8,6 +9,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useFocusEffect } from '@react-navigation/native';
 
 import * as Styled from './styles';
+import { LoadingContainer } from './styles';
+import { useTheme } from 'styled-components';
 
 export interface DataListProps extends TransactionCardProps {
   id: string;
@@ -23,6 +26,7 @@ interface HighlightData {
 }
 
 export function Dashboard() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [transactions, setTransactions] = useState<DataListProps[]>([]);
   const [highlightData, setHighlightData] = useState<HighlightData>({
     entries: { value: '' },
@@ -38,7 +42,6 @@ export function Dashboard() {
     let entriesTotalSum = 0;
     let totalExpenses = 0;
     let total = 0;
-
 
     const transactionsFormatted: DataListProps[] = transactions.map(
       (item: DataListProps) => {
@@ -59,7 +62,7 @@ export function Dashboard() {
           month: '2-digit',
           year: '2-digit',
         }).format(new Date(item.date));
-        
+
         return {
           id: item.id,
           date,
@@ -91,65 +94,78 @@ export function Dashboard() {
         }),
       },
     });
+    setIsLoading(false);
   }
 
   useEffect(() => {
     fetchTransactions();
   });
 
-  useFocusEffect(useCallback(() => {
-    fetchTransactions();
-  }, []))
+  useFocusEffect(
+    useCallback(() => {
+      fetchTransactions();
+    }, [])
+  );
+
+  const theme = useTheme();
 
   return (
     <Styled.Container>
-      <Styled.Header>
-        <Styled.UserWrapper>
-          <Styled.UserInfo>
-            <Styled.Photo
-              source={{
-                uri: 'https://avatars.githubusercontent.com/u/62480134?v=4',
-              }}
-            />
-            <Styled.User>
-              <Styled.UserGreeting>Olá, </Styled.UserGreeting>
-              <Styled.UserName>Lourenço</Styled.UserName>
-            </Styled.User>
-          </Styled.UserInfo>
-          <Styled.LogoutButton onPress={() => {}}>
-            <Styled.Icon name='power' />
-          </Styled.LogoutButton>
-        </Styled.UserWrapper>
-      </Styled.Header>
+      {isLoading ? (
+        <LoadingContainer>
+          <ActivityIndicator color={theme.colors.primary} size="large" />
+        </LoadingContainer>
 
-      <Styled.HighlightCards>
-        <HighLightCard
-          title='Entrada'
-          amount={highlightData.entries.value}
-          lastTransaction='Última entrada dia 13 de abril'
-          type='up'
-        />
-        <HighLightCard
-          title='Saída'
-          amount={highlightData.expenses.value}
-          lastTransaction='Última entrada dia 13 de abril'
-          type='down'
-        />
-        <HighLightCard
-          title='Total'
-          amount={highlightData.total.value}
-          lastTransaction='01 a 16 de abril'
-          type='total'
-        />
-      </Styled.HighlightCards>
-      <Styled.Transactions>
-        <Styled.Title>Listagem</Styled.Title>
-        <Styled.TransactionsList
-          data={transactions}
-          keyExtractor={(item: DataListProps) => item.id}
-          renderItem={({ item }) => <TransactionCard data={item} />}
-        />
-      </Styled.Transactions>
+      ) : (
+        <>
+          <Styled.Header>
+            <Styled.UserWrapper>
+              <Styled.UserInfo>
+                <Styled.Photo
+                  source={{
+                    uri: 'https://avatars.githubusercontent.com/u/62480134?v=4',
+                  }}
+                />
+                <Styled.User>
+                  <Styled.UserGreeting>Olá, </Styled.UserGreeting>
+                  <Styled.UserName>Lourenço</Styled.UserName>
+                </Styled.User>
+              </Styled.UserInfo>
+              <Styled.LogoutButton onPress={() => { }}>
+                <Styled.Icon name='power' />
+              </Styled.LogoutButton>
+            </Styled.UserWrapper>
+          </Styled.Header>
+          <Styled.HighlightCards>
+            <HighLightCard
+              title='Entrada'
+              amount={highlightData.entries.value}
+              lastTransaction='Última entrada dia 13 de abril'
+              type='up'
+            />
+            <HighLightCard
+              title='Saída'
+              amount={highlightData.expenses.value}
+              lastTransaction='Última entrada dia 13 de abril'
+              type='down'
+            />
+            <HighLightCard
+              title='Total'
+              amount={highlightData.total.value}
+              lastTransaction='01 a 16 de abril'
+              type='total'
+            />
+          </Styled.HighlightCards>
+          <Styled.Transactions>
+            <Styled.Title>Listagem</Styled.Title>
+            <Styled.TransactionsList
+              data={transactions}
+              keyExtractor={(item: DataListProps) => item.id}
+              renderItem={({ item }) => <TransactionCard data={item} />}
+            />
+          </Styled.Transactions>{' '}
+        </>
+      )}
     </Styled.Container>
   );
 }
